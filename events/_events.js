6fe4bbeaa73621040ba569f54663ee062d2d9f5a
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2025-01-13 22:13:44 +0100

    Combine event files into one file

---
+++ events/_events.js
@@ -0,0 +1,126 @@
+import { getRandomItemFromArray, pushToDataLayer, showToast, generateRandomInteger } from '../functions.js'
+
+export default function handleEvents() {
+  const forms = document.querySelectorAll('form')
+
+  if (!forms.length) {
+    return
+  }
+
+  forms.forEach(form => {
+    form.addEventListener('submit', e => {
+      try {
+        e.preventDefault()
+
+        const eventName = form?.dataset?.eventName || null
+
+        if (!eventName) {
+          throw Error(`Event name not found on form.`)
+        }
+
+        // prettier-ignore
+        const acceptableEventNames = [
+          'login',
+          // 'purchase',
+          'search',
+          'select_content',
+          'share',
+          'sign_up'
+        ]
+
+        if (!acceptableEventNames.includes(eventName)) {
+          throw Error('Unrecognized event name')
+        }
+
+        const methods = ['Email', 'Google', 'Facebook', 'Twitter/X', 'LinkedIn', 'Apple']
+        const content_types = ['blog', 'discount', 'coupon', 'product', 'page']
+        const id = generateRandomInteger()
+
+        let dataLayerObject = null
+
+        switch (eventName) {
+          case 'login':
+            dataLayerObject = {
+              event: eventName,
+              method: getRandomItemFromArray(methods), // string|optional
+            }
+            break
+
+          case 'search':
+            const searchValue = form?.elements?.search?.value || ''
+            const regionValue = form?.elements?.region?.value || ''
+
+            dataLayerObject = {
+              event: eventName,
+              search_term: searchValue,
+              region: regionValue, // custom parameter/dimension
+            }
+            break
+
+          case 'select_content':
+            dataLayerObject = {
+              event: eventName,
+              content_type: getRandomItemFromArray(content_types),
+              content_id: id,
+            }
+            break
+
+          case 'share':
+            dataLayerObject = {
+              event: eventName,
+              method: getRandomItemFromArray(methods),
+              content_type: getRandomItemFromArray(content_types),
+              item_id: id,
+            }
+            break
+
+          case 'sign_up':
+            dataLayerObject = {
+              event: eventName,
+              method: getRandomItemFromArray(methods),
+            }
+            break
+
+          // case '':
+          //   dataLayerObject =
+          //   break
+
+          // case '':
+          //   dataLayerObject =
+          //   break
+
+          // case '':
+          //   dataLayerObject =
+          //   break
+        }
+
+        if (Object.keys(dataLayerObject).length === 0) {
+          throw Error('dataLayerObject is empty')
+        }
+
+        pushToDataLayer(dataLayerObject)
+
+        form.reset()
+
+        showToast('Success')
+      } catch (error) {
+        // Adding to dataLayer so that GTM can pick it up
+        const errorInfo = {
+          event: 'log_error',
+          error_message: error?.message || '',
+          error_stack: error?.stack || '',
+          form_id: form?.id || '',
+          form_name: form?.name || '',
+          page_title: form?.elements['title']?.value || '',
+          page_permalink: form?.elements['permalink']?.value || '',
+        }
+
+        console.log(errorInfo)
+        console.log(error)
+
+        showToast(error?.message || 'There was an error. Check Console.')
+        pushToDataLayer(errorInfo)
+      }
+    })
+  })
+}
--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-import search from './events/search.js'
-search()
+// import search from './events/search.js'
+// search()
 
-import sign_up from './events/sign_up.js'
-sign_up()
+// import sign_up from './events/sign_up.js'
+// sign_up()
 
-import login from './events/login.js'
-login()
+// import login from './events/login.js'
+// login()
 
-import share from './events/share.js'
-share()
+// import share from './events/share.js'
+// share()
 
-import select_content from './events/select_content.js'
-select_content()
+// import select_content from './events/select_content.js'
+// select_content()
+
+import handleEvents from './events/_events.js'
+handleEvents()
