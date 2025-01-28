8ee661652fce505fa2d9ff469be79703d87b3ec1
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2025-01-28 14:18:09 +0100

    Clean up scripts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,2 @@
-// import search from './events/search.js'
-// search()
-
-// import sign_up from './events/sign_up.js'
-// sign_up()
-
-// import login from './events/login.js'
-// login()
-
-// import share from './events/share.js'
-// share()
-
-// import select_content from './events/select_content.js'
-// select_content()
-
 import handleEvents from './events/_events.js'
 handleEvents()
